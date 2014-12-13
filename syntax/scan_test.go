554ea1bbfad5b3c68f3db6df35aecd44a72302ac
huangml <huangml.zh@gmail.com>
2014-12-13 12:08:24 +0800

    parser

--- syntax/scan_test.go
+++ syntax/scan_test.go
@@ -15,38 +15,28 @@ type Token struct {
 	Column int
 }
 
-var text = `
-hello321.456 world
+var identRegexp = regexp.MustCompile(`[a-zA-Z0-9\.\-_\(\)]`)
 
-message haha 123 {
-	optional int32 x = 100[abc=def]; // fdas fdsa fdsa
+func isIdent(s string) bool {
+	return identRegexp.MatchString(s)
 }
 
-// abc
-// def
-
-set x = "string";
+var text = `
+default=}100
 `
 
 func TestA(t *testing.T) {
-	for _, v := range scan(bytes.NewReader([]byte(text))) {
-		fmt.Println(v)
-	}
+	toks := scan(bytes.NewReader([]byte(text)))
+	p := NewParser(toks)
+	fmt.Println(p.parseOption())
 }
-
-func wordRune(ch rune, i int) bool {
-	return ch >= 'a' && ch <= 'z' || ch >= 'A' && ch <= 'Z' || ch >= '0' && ch <= '9' ||
-		ch == '_' || ch == '-' || ch == '.'
-}
-
-var regex = regexp.MustCompile(`[a-zA-Z0-9.-_]`)
 
 func scan(r io.Reader) (toks []Token) {
 	var s scanner.Scanner
 	s.Init(r)
 	s.Mode = scanner.ScanIdents | scanner.ScanStrings | scanner.ScanComments
 	s.Whitespace = scanner.GoWhitespace
-	s.IsIdentRune = wordRune
+	s.IsIdentRune = func(c rune, i int) bool { return isIdent(string(c)) }
 
 	for {
 		if t := s.Scan(); t != scanner.EOF {
@@ -61,19 +51,88 @@ func scan(r io.Reader) (toks []Token) {
 	}
 }
 
+type Parser struct {
+	toks  []Token
+	index int
+}
+
+func NewParser(toks []Token) *Parser {
+	return &Parser{
+		toks: toks,
+	}
+}
+
+func (p *Parser) EOF() bool {
+	return p.index >= len(p.toks)
+}
+
+func (p *Parser) peek() Token {
+	return p.toks[p.index]
+}
+
+func (p *Parser) next() Token {
+	defer func() { p.index++ }()
+	return p.toks[p.index]
+}
+
 // key=value
 type Option struct {
 	Left, Right Token
 }
 
-// tok1 tok2;
+func (p *Parser) parseOption() (opt *Option, err error) {
+	when := "parseOption"
+	checkEOF := func() bool {
+		if p.EOF() {
+			opt, err = nil, EOFError(when)
+			return false
+		}
+		return true
+	}
+
+	var t [3]Token
+	for i := range t {
+		if !checkEOF() {
+			return
+		}
+		t[i] = p.next()
+	}
+
+	if !isIdent(t[0].Text) {
+		return nil, TokenError(when, t[0])
+	}
+
+	if t[1].Text != "=" {
+		return nil, TokenError(when, t[1])
+	}
+
+	if !isIdent(t[2].Text) {
+		return nil, TokenError(when, t[2])
+	}
+
+	return &Option{
+		Left:  t[0],
+		Right: t[2],
+	}, nil
+}
+
+func EOFError(when string) error {
+	return fmt.Errorf("unexpected EOF when %v", when)
+}
+
+func TokenError(when string, tok Token) error {
+	return fmt.Errorf("unexepcted token '%v' when %v [%v:%v]", tok.Text, when, tok.Line, tok.Column)
+}
+
+// tok1 tok2
 type Declare struct {
 	Token []Token
 }
 
-// left1 left2 = right1 right2 [key = value];
+// left1 left2 = right1[key=value]
 type Equation struct {
-	Left, Right []Token
+	Left  []Token
+	Right Token
 	*Option
 }
 
